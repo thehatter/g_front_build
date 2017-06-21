a22538804c9073f6ffedcf5f2af677c4f10c0c87
thehatter <andrewlutenko@gmail.com>
2017-06-21 13:29:51 +0300

    initial commit

---
+++ scss/mobile_main.js
@@ -0,0 +1,21 @@
+// Third-party
+@import "vendor/bootstrap.min.css";
+@import "vendor/font-awesome.min.css";
+@import "vendor/bootstrap.offcanvas.min.css";
+@import "vendor/owl.carousel.min.css";
+@import "vendor/owl.theme.default.min.css";
+@import "vendor/featherlight.min.css";
+@import "vendor/lytebox.css";
+@import "vendor/lightbox.css";
+@import "vendor/jquery-ui.css";
+@import "vendor/jquery-ui.structure.css";
+@import "vendor/jquery.countdown.css";
+
+// Modules and Variables
+@import "base/mobile_main.css";
+
+// Partials
+@import "components/ad_sale_pages.scss";
+@import "components/new_video_vidget.scss";
+
+// ...
